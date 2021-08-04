4ca5bbc703fe6ccb87598b60c7bac7289f305c4e
Rishabh Varshney <rishabhvarshney14@gmail.com>
2021-08-04 17:36:35 +0530

    added env port in index

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use("/", Routes);
 
 // Port
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // Connecting to Database
 const username = process.env.DB_USERNAME;
